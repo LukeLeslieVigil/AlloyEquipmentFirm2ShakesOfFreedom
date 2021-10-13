6eac8397324175425897ebb1732ea03b1e3825ef
LukeLeslieVigil <seventaco123@gmail.com>
2021-10-13 14:30:23 -0600

    Added new scripts
    Added random shake spawning script, shake destroy when it gets picked up, and it adds 1 to score when shake is destroyed depending on tag of object.

---
+++ Assets/Scripts/ShakeCollect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeCollect : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject shake;
+    [SerializeField]
+    private GameObject gameController;
+
+    private ScoreCounter scoreCounter;
+
+    private void Start()
+    {
+        gameController = GameObject.FindWithTag("GameController");
+        scoreCounter = gameController.GetComponent<ScoreCounter>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            scoreCounter.playerScore += 1;
+        }
+        Destroy(shake);
+    }
+}
