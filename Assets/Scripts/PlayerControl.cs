7067a5ff5b92b26834b542d810bb79bcd6fdde9d
LukeLeslieVigil <seventaco123@gmail.com>
2021-10-17 13:41:03 -0600

    Game Area fully implemented
    Game area is now fully implemented with updated shake spawning locations, in game menu scripts, and
    better character controlling

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -6,11 +6,12 @@ public class PlayerControl : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private float gravityValue = -9.81f;
+    private Vector3 rotation;
 
     [SerializeField]
     private float playerSpeed = 2.0f;
+    [SerializeField]
+    private float rotationSpeed = 180f;
 
     private void Start()
     {
@@ -19,24 +20,25 @@ public class PlayerControl : MonoBehaviour
 
     void Update()
     {
-        //Checks if the player is grounded so the player doesn't go flying off into space
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
         //Gets the inputs from the keyboard set using project settings
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        //Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //controller.Move(move * Time.deltaTime * playerSpeed);
 
         //Moves the player forward based on inputs
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
+        //if (move != Vector3.zero)
+        //{
+        //    gameObject.transform.forward = move;
+        //}
 
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        //controller.Move(playerVelocity * Time.deltaTime);
+
+        //Gets the rotation vector and the forward and backwards rotation vector
+        this.rotation = new Vector3(0, Input.GetAxisRaw("Horizontal") * rotationSpeed * Time.deltaTime, 0);
+        Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
+
+        //Then moves the character in the actual direction and allows it to rotate as it moves
+        move = this.transform.TransformDirection(move);
+        controller.Move(move * playerSpeed);
+        this.transform.Rotate(this.rotation);
     }
 }
